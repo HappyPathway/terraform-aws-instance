83434c7d3345b836afaee1b3cdb0b0b7416bd2f3
djaboxx <dave@roknsound.com>
2024-08-22 06:06:08 +0000

    AutoFormat Commit

--- iam.tf
+++ iam.tf
@@ -11,7 +11,7 @@ data "aws_iam_policy_document" "assume_role" {
 }
 
 resource "aws_iam_role" "instance" {
-    name               = lookup(var.required_tags, "Name")
+  name               = lookup(var.required_tags, "Name")
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
