f4e913fb88c448b83cbfe5dc0d9fd3d0c9caddf6
David Arnold <10138997+djaboxx@users.noreply.github.com>
2024-08-21 22:49:19 -0700

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
   linux_instance       = var.ssh_user == null ? false : true
-  windows_instance     = var.windows_instance
+    windows_instance     = var.windows_instance
   iam_instance_profile = var.iam_instance_profile ? var.iam_instance_profile : var.iam_policy == null ? null : aws_iam_instance_profile.instance.name
 }
